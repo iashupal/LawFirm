a41f4df5719fcce68ebbe424b768b8c04f3f02ca
Abhishek Kalia <abhishek.kalia@outlook.com>
2019-06-13 12:54:20 +0530

    Update component demo

--- src/containers/ComponentDemo.js
+++ src/containers/ComponentDemo.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import Splitter from '../components/Splitter';
-function ComponentDemo() {
-    return (
-        <div>
-            <Splitter />
-        </div>
-    );
+
+class ComponentDemo extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+    render() {
+        return <div>ComponentDemo</div>;
+    }
 }
 
 export default ComponentDemo;
