a85eabde9d9eea203494fffcd37b738b7025023d
iashupal <ashupal0005@gmail.com>
2019-06-13 12:25:48 +0530

    add splitter component

--- src/components/Splitter/index.js
+++ src/components/Splitter/index.js
@@ -1,63 +1,15 @@
-// import React from 'react';
-// import {Splitter as SplitScreen} from 'm-react-splitters';
-// import 'm-react-splitters/lib/splitters.css';
-
-// export default function SplitScreen({ children }) {
-//     return (
-//         <SpliterScreen
-//             position="horizontal"
-//             primaryPaneMaxHeight="80%"
-//             primaryPaneMinHeight={0}
-//             primaryPaneHeight="400px"
-//             dispatchResize={true}
-//             postPoned={true}
-//         >
-//             <SpliterScreen
-//                 position="vertical"
-//                 primaryPaneMaxWidth="80%"
-//                 primaryPaneMinWidth={0}
-//                 primaryPaneWidth="400px"
-//                 postPoned={false}
-//             >
-//                 {children}
-//             </SpliterScreen>
-//         </SpliterScreen>
-//     );
-// }
-
-
 import React from 'react';
-import Splitter from 'm-react-splitters';
-import 'm-react-splitters/lib/splitters.css';
-class Spliter extends React.Component {
-
-  render() {
+import SplitterLayout from 'react-splitter-layout';
+import EnhancedTable from '../Table/EnhancedTable';
+import 'react-splitter-layout/lib/index.css';
+export default function Splitter(){
     return (
-    <Splitter
-        position="horizontal"
-        primaryPaneMaxHeight="80%"
-        primaryPaneMinHeight={0}
-        primaryPaneHeight="400px"
-        dispatchResize={true}
-        postPoned={true}
-    >    
-        <Splitter
-            position="vertical"
-            primaryPaneMaxWidth="80%"
-            primaryPaneMinWidth={0}
-            primaryPaneWidth="400px"
-            postPoned={false}
-        >    
-            <div></div>
-            <div></div>
-            
-
-        </Splitter> 
-        <div></div>
-    </Splitter>
+  
+    <SplitterLayout className={classes.spliter}>
+        <div><EnhancedTable/></div>
+        <div>Pane 2</div>
+      </SplitterLayout>
 
     );
-  }
 }
  
-export default Spliter;
--- src/containers/ComponentDemo.js
+++ src/containers/ComponentDemo.js
@@ -1,32 +1,9 @@
 import React from 'react';
-import Button from '../components/Button';
-import PageTitle from '../components/PageTitle';
-import EnhancedTable from '../components/Table/EnhancedTable';
 import Splitter from '../components/Splitter';
-import CustomAvatar from '../components/CustomAvatar';
 function ComponentDemo() {
     return (
         <div>
-            <Button 
-                mode="rightIcon" 
-                size="medium" 
-                color="primary"
-                icon="add_to_queue"
-            >
-                Hello
-            </Button>
-            <PageTitle icon="class" text="Case" iconVisible/>
-          
-           <EnhancedTable/>
            <Splitter/>
-            <CustomAvatar color="blue">
-                <i className="material-icons" style={{fontSize: '20px'}}>add_to_queue</i>
-            </CustomAvatar>
-            <CustomAvatar color="pink">
-                H
-            </CustomAvatar>
-          
-          
         </div>
     );
 }
